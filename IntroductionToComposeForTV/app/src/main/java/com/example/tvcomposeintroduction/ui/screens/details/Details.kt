e434343dcd09092832b1c63324511a6af1c1636c
Dandan Meng <mengdandanno1@163.com>
2024-08-28 21:45:22 +0800

    4: Implement the details screen

--- IntroductionToComposeForTV/app/src/main/java/com/example/tvcomposeintroduction/ui/screens/details/Details.kt
+++ IntroductionToComposeForTV/app/src/main/java/com/example/tvcomposeintroduction/ui/screens/details/Details.kt
@@ -16,10 +16,64 @@
 
 package com.example.tvcomposeintroduction.ui.screens.details
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.tv.material3.MaterialTheme
+import androidx.tv.material3.Text
+import coil.compose.AsyncImage
 import com.example.tvcomposeintroduction.data.Movie
 
 @Composable
 fun Details(movie: Movie, modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        AsyncImage(
+            model = movie.cardImageUrl,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+        Box(
+            modifier = Modifier
+                .background(
+                    Brush.linearGradient(
+                        listOf(
+                            MaterialTheme.colorScheme.background,
+                            Color.Transparent
+                        )
+                    )
+                )
+                .fillMaxSize()
+        ) {
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 48.dp, vertical = 24.dp)
+                    .fillMaxWidth(0.5f)
+            ) {
+                Text(
+                    text = movie.title,
+                    style = MaterialTheme.typography.displayMedium,
+                )
+                Text(
+                    text = movie.studio,
+                    style = MaterialTheme.typography.bodySmall,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(
+                    text = movie.description,
+                )
+            }
+        }
+    }
 }
